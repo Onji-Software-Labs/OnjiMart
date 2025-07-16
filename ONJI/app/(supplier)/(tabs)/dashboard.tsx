a9c9cca91e6ec7b52aacee89266a71b0b56d2250
Prajwalchandru85 <prajwal3.mitmpl2022@learner.manipal.edu>
2025-07-17 00:20:14 +0530

    API integration-login and OTP verification

--- ONJI/app/(supplier)/(tabs)/dashboard.tsx
+++ ONJI/app/(supplier)/(tabs)/dashboard.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Dashboard() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-base">Dashboard screen</Text>
+      <Text className="text-base">home screen</Text>
     </View>
   );
 }
