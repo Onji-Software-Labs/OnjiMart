261ff569970b799a110c5183c213739dbb78aeb3
admin-onji <admin@onjisoftware.com>
2025-07-28 01:36:03 +0530

    chore: remove unused code

--- src/main/java/com/sattva/service/AuthenticationService.java
+++
@@ -1,7 +0,0 @@
-package com.sattva.service;
-
-public interface AuthenticationService {
-    boolean validatePhoneNumberAndOtp(String phoneNumber, int otp);
-    boolean validatePhoneNumberAndOtpLess(String orderId, int otp,String phoneNumber);
-    String generateToken(String phoneNumber, String userId);
-}
