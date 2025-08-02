fef467cf41b14b752a2f5adf4b92d8293f7f4398
AnishJathan <56048276+AnishJathan@users.noreply.github.com>
2025-08-02 21:44:30 +0530

    n

--- src/main/java/com/sattva/model/User.java
+++ src/main/java/com/sattva/model/User.java
@@ -45,7 +45,7 @@ public  class User implements UserDetails {
     @UuidGenerator
     private String id;
 
-    @Column(unique = true)
+    @Column(name = "full_name")
     private String email;
 
     @Column(name = "full_name")
