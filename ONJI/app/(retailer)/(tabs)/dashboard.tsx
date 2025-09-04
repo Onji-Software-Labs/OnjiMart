a185fa4a8e2dd75b5d5dbc1325336f892a2b001a
Shettyvibha <146423987+Shettyvibha@users.noreply.github.com>
2025-09-04 18:17:56 +0530

    Delete ONJI/app/(retailer)/(tabs)/dashboard.tsx

--- ONJI/app/(retailer)/(tabs)/dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Dashboard() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-base">home screen</Text>
-    </View>
-  );
-}
