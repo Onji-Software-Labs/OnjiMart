050de0a8003d8fe0c4eef22aaee1187f083e3a95
Shettyvibha <146423987+Shettyvibha@users.noreply.github.com>
2025-09-04 18:16:25 +0530

    Delete ONJI/app/(retailer)/(tabs)/cart.tsx

--- ONJI/app/(retailer)/(tabs)/cart.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Cart() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-lg font-semibold">Cart screen</Text>
-    </View>
-  );
-}
