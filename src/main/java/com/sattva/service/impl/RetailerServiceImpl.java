7b0301e8d0e7b5cfbbb788050c82a70184922fe5
INFOKOM <ahlemboubakri50@gmail.com>
2026-05-22 13:02:34 +0100

    Add product images in order screen

--- src/main/java/com/sattva/service/impl/RetailerServiceImpl.java
+++ src/main/java/com/sattva/service/impl/RetailerServiceImpl.java
@@ -195,6 +195,7 @@ public class RetailerServiceImpl implements RetailerService {
                 ));
 
         user.setUserType(UserType.valueOf(dto.getUserType().toUpperCase()));
+        user.setUserOnboardingStatus(true);
         userRepository.save(user);
 
         if (retailer.getRetailerBusinesses() != null && !retailer.getRetailerBusinesses().isEmpty()) {
@@ -226,10 +227,6 @@ public class RetailerServiceImpl implements RetailerService {
 
         Retailer savedRetailer = retailerRepository.save(retailer);
 
-        if (!user.isUserOnboardingStatus()) {
-            user.setUserOnboardingStatus(true);
-            userRepository.save(user);
-        }
 
         return modelMapper.map(savedRetailer, RetailerDTO.class);
     }
